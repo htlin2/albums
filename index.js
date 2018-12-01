c0a197a43124762b8eec3f4d2a1d08af31da6d43
htlin2 <htlin2@gmail.com>
2018-12-01 15:26:42 -0800

    Updated project setup

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-/** @format */
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/header';
+// import AlbumList from './src/components/AlbumList';
+import { name as appName } from './app.json';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+const App = () => (
+  <View style={{ flex: 1 }}>
+    <Header headerText={'Albums'} />
+    {/* <AlbumList /> */}
+  </View>
+);
 
 AppRegistry.registerComponent(appName, () => App);
